4aad7c5e0141e8abc536f8728d19df8a6e68c1d3
s-acosta <68660572+s-acosta@users.noreply.github.com>
2022-03-10 13:11:23 -0800

    Major update setters getters
    - Added setters and getters methods
    - Overall changes to readability
    - Changes to the the way input options are feeded to the class

--- NeurotarExtractor.m
+++ NeurotarExtractor.m
@@ -7,135 +7,306 @@ classdef NeurotarExtractor < handle
     end
     
     properties (Access = private)
-        options
-        tdms_filename
+        FILENAME
+        VERSION = 'new'
+        OFF_TIME = 0
+        SAMPLING_PERIOD = 100
+        TDMS_FLAG = true
+        FILETYPE
+        FIELDS = ["X", "Y", "zones", "time", "speed", "R", "phi", ...
+            "alpha", "omega"]
+    end
+    
+    properties (Dependent)
+        PARAMETERS
+        pose
     end
     
     % Initializing methods
     methods (Access = public)
         
-        function obj = NeurotarExtractor(tdms_filename, varargin)
-            
-            obj.options = inputParser;
-            addParameter(obj.options, 'IMAGING_METHOD','WideField')
-            addParameter(obj.options, 'SAMPLING_PERIOD', 100)                   % changed from "samplingWindow", default 100 (msec?)
-            addParameter(obj.options, 'OFF_TIME', 0)
-            addParameter(obj.options, 'VERSION', 'New')
-            parse(obj.options,varargin{:})
+        function obj = NeurotarExtractor(input_file)
             
             if nargin == 0
-                tdms_filename = obj.getTDMSFilename();
+                obj.getFilename();
+            else
+                obj.FILENAME = input_file;
             end
             
-            obj.tdms_filename = obj.checkTDMSFilename(tdms_filename);           % added to allow flexibility for either filenames or not
-            obj.readTDMS();
             
-            obj.resampling();
-            obj.movingDetector();
+            obj.readFile();
+             
+%             obj.resampling();
+%             obj.movingDetector();
+
+        end
+        
+    end
+         
+    methods
+        
+        function setPropValue(obj, prop_name, prop_value)
+            % If more than one property value, prop name and prop value
+            % must be column vectors
+            eid = 'getPropValue:inputError';
+            
+            if nargin == 2 
+                
+                if isstruct(prop_name)
+                
+                prop_struct = prop_name;
+                fn = fieldnames(prop_struct);
+                [prop_name, prop_value] = deal(strings([1,length(fn)]));
+                
+                for i = 1:length(fn)
+                    prop_name(i) = fn{i};
+                    prop_value(i) = string(prop_struct.(prop_name(i)));
+                end
+                
+                else
+                    
+                    msg = 'If one input is given, it must be a struct';
+                    throwAsCaller(MException(eid, msg));
+                end
+       
+            end
+                   
+            if ~isstring(prop_name)
+                throwAsCaller(MException(eid, 'Prop name must be a string'));
+                
+            elseif ~isstring(prop_value)
+                throwAsCaller(MException(eid, 'Value name must be a string'));
+                
+            else
+                for i = size(prop_name, 2)
+                    obj.(prop_name(i)) = prop_value(i);
+                end
+                
+            end
+             
+        end
+             
+        function set.VERSION(obj, input)
+            
+            eid = 'VERSION:inputError';
+            msg = 'Only "old" and "new" version names accepted';
+            
+            if isstring(input)
+                input = convertStringsToChars(input);
+                
+            elseif ~ischar(input)
+                throwAsCaller(MException(eid,msg));
+            end
+            
+            if ~ismember(input, {'old', 'new'})
+                throwAsCaller(MException(eid,msg))
+            else
+                obj.VERSION = input;
+            end
+            
+        end
+        
+        function set.OFF_TIME(obj, input)
+
+            msg = 'OFF_TIME must be a number';
+            eid = 'OFF_TIME:inputError';
+            
+            input = obj.checkPositiveInteger(input, msg, eid);
+            obj.OFF_TIME = input;
+            
+        end
+        
+        function set.SAMPLING_PERIOD(obj, input)
+            
+            msg = 'SAMPLING_PERIOD must be a number';
+            eid = 'SAMPLING_PERIOD:inputError';
+            
+            input = obj.checkPositiveInteger(input, msg, eid);
+            obj.SAMPLING_PERIOD = input;
+            
+        end
+        
+        function set.FILENAME(obj, input)
+            
+            eid = 'TDMS_FILENAME:inputError';
+            msg = 'Input file must be TDMS/XLSX';
+            
+            if isstring(input)
+                input = convertStringsToChars(input);
+            end
+            
+            if ~ischar(input)
+                throwAsCaller(MException(eid,msg)) 
+            end
+            
+            [~, ~, ext] = fileparts(input);
+            if strcmp(ext, '.tdms') || strcmp(ext, '.xlsx')
+                obj.FILENAME = input;
+            else 
+                throwAsCaller(MException(eid,msg))                        
+            end
+            
+        end
+        
+        function parameters = get.PARAMETERS(obj)
+            
+            parameters = struct;
+            parameters.VERSION = obj.VERSION;
+            parameters.OFF_TIME = obj.OFF_TIME;
+            parameters.FILENAME = obj.FILENAME;
+            parameters.SAMPLING_PERIOD = obj.SAMPLING_PERIOD;
+            
+        end
+        
+        function pose = get.pose(obj)
+            
+            if ~isempty(obj.behavior)
+                pose = [obj.behavior.X; obj.behavior.Y; ...
+                        wrapTo360(obj.behavior.alpha - 90)];
+            elseif ~isempty(obj.behavior_raw)
+                pose = [obj.behavior_raw.X'; obj.behavior_raw.Y'; ...
+                        wrapTo360(obj.behavior_raw.alpha' - 90)];
+            else
+                pose = [];
+                disp('Raw file is yet to be read')
+            end
+                
+  
         end
         
     end
     
-    % Convert from .tdms methods. TDMS functions need to be in path
+    methods(Static, Access = private)
+        
+        function out = checkPositiveInteger(input, msg, eid)
+            
+            if isstring(input)
+                input = str2double(input);
+            end
+            
+            if isnan(input)
+                
+                throwAsCaller(MException(eid,msg))
+                
+            elseif input<0 || mod(input,1)
+                
+                throwAsCaller(MException(eid,msg))   
+            else
+                out = input;
+            end
+            
+            
+        end
+        
+    end
+        
+    % Read from TDMS or XLSX files. If TDMS, TDMS functions need to be in path
     methods (Access = private)
         
-        function out = checkTDMSFilename(obj, tdms_filename)
+        function getFilename(obj)
             
-            [~, ~, ext] = fileparts(tdms_filename);
-            if strcmp(ext, '.tdms')
-                out = tdms_filename;
-                return % is a proper tdms file
+            folder = uigetdir([], 'Choose the folder containing the file:');
+            
+            tdms_files = strcat(folder, filesep, '*.tdms');
+            xlsx_files = strcat(folder, filesep, '*.xlsx');
+            file_location = [dir(tdms_files); dir(xlsx_files)];
+            
+            if numel(file_location) > 1
+                warning('Multiple TDMS/XLSX files found, choose your file: ')
+                [tdms_fn, tdms_pn] = uigetfile(strcat(folder, filesep,...
+                    '*.tdms;*.xlsx'));
+                filename = strcat(tdms_pn, filesep, tdms_fn);
             else
-                obj.enforceSessionName(tdms_filename);                          % confirm that it's a session name, not some other junk
-                out = strcat(tdms_filename, '.tdms');                           % properly convert from session name into tdms filename
+                filename = strcat(file_location.folder, filesep, ...
+                    file_location.name);
             end
+            
+            obj.FILENAME = filename;
             
         end
         
-        function enforceSessionName(obj, tdms_filename)                         % used to be called checkIDName
-            % think about this section...
+        function readFile(obj)
             
-            if isempty(regexp(tdms_filename,...
-                    '[A-Z][A-Z][A-Z][0-9][0-9][0-9]_[0-9][0-9][0-1][0-9][0-3][0-9]', 'once')) % enforce format
-                error('Session ID incorrect, should be in the format: NNN###_YYMMDD, examples: SAM000_210910');
+            [~, ~, data_format] = fileparts(obj.FILENAME);
+            switch data_format
+                
+                case '.xlsx'
+                    obj.readXLSX();
+                    
+                case '.tdms'
+                    obj.readTDMS();
+                    
             end
-  
-        end
-        
-        function tdms_filename = getTDMSFilename(obj)
-            
-            fprintf('Choose your folder containing the .tdms file: \n');
-            folder = uigetdir();
-            
-            tdms_location = dir(strcat(folder, filesep, '*.tdms'));
-            if numel(tdms_location) > 1
-                warning('Multiple TDMS files found, choose your TDMS file: ')
-                [tdms_fn, tdms_pn] = uigetfile('*.tdms');
-                tdms_filename = strcat(tdms_pn, filesep, tdms_fn);
-            else
-                tdms_filename = strcat(tdms_location.folder, filesep, tdms.name);
-            end
-            
+
         end
         
         function readTDMS(obj)
             
-            behavior_file = TDMS_readTDMSFile(obj.tdms_filename);
-           
+            if ~exist('TDMS_readTDMSFile', 'file')
+               error(strcat('Error. TDMS_readTDMSFile() not found.', ...
+                   ' Please add it to the path. You can get it at ', ...
+                   'https://www.mathworks.com/matlabcentral/',...
+                   'fileexchange/30023-tdms-reader'))
+            end
+            
+            behavior_file = TDMS_readTDMSFile(obj.FILENAME);
+            
             behavior_data = behavior_file.data;
             obj.behavior_raw = struct;
             
-            switch obj.options.Results.VERSION
+            switch obj.VERSION
                 
-                case 'New'
+                case 'new'
+                    
                     obj.behavior_raw.R = behavior_data{8}';
                     obj.behavior_raw.phi = behavior_data{9}';
                     obj.behavior_raw.alpha = behavior_data{10}';
                     
                     obj.behavior_raw.X = behavior_data{11}';
                     obj.behavior_raw.Y = -behavior_data{12}';
+                    
                     if max(abs(obj.behavior_raw.X)) < 100 ...                   % In some neutorar versions, X and Y are normalized to 100,
                             && max(abs(obj.behavior_raw.Y)) < 100
                         obj.behavior_raw.X = 1.25 * obj.behavior_raw.X;
-                        obj.behavior_raw.Y = 1.25 * obj.behavior_raw.Y; 
+                        obj.behavior_raw.Y = 1.25 * obj.behavior_raw.Y;
                     end
                     
                     obj.behavior_raw.theta = behavior_data{13}';
                     obj.behavior_raw.beta = behavior_data{14}';
                     
-                    
-                case 'Old'
+                case 'old'
                     obj.behavior_raw.R = behavior_data{10}';
                     obj.behavior_raw.phi = behavior_data{11}';
                     obj.behavior_raw.alpha = behavior_data{12}';
                     obj.behavior_raw.X = behavior_data{13}';
                     obj.behavior_raw.Y = -behavior_data{14}';
-
-                   
-                otherwise
-                    answer = questdlg('Which NeuroTar version are you using?',...
-                        'NeuroTar Version','Old', 'New', 'New');
-                    
-                    obj.options.Results.VERSION = answer;
-                    readTMDS(obj);
                     
             end
             
             obj.behavior_raw.time = behavior_data{5};
-                    obj.behavior_raw.time = obj.behavior_raw.time - ...
-                        obj.behavior_raw.time(1);
+            obj.behavior_raw.time = obj.behavior_raw.time - ...
+                obj.behavior_raw.time(1);
             obj.behavior_raw.w = behavior_data{15}';
             obj.behavior_raw.speed = behavior_data{16}';
             obj.behavior_raw.zones = behavior_data{17}';
             
         end
         
+        function readXLSX(obj)
+            
+            disp('reading XLSX')
+            [~, ~, excel_data] = xlsread(obj.FILENAME, 3);
+            
+            behavior_raw = struct;
+ 
+        end
+        
     end
     
     % Postprocessing methods
-    methods (Access = private)
+    methods (Access = public)
         
-        function resampling(obj)
+        function reSample(obj)
             
             % This function takes the Neurotar sampling rate (typically 20ms or
             % 13ms) and resamples it taking into account the sampling rate of
@@ -144,8 +315,8 @@ classdef NeurotarExtractor < handle
             
             obj.behavior = struct;
             
-            microscope_rate = obj.options.Results.SAMPLING_PERIOD;
-            off_time = obj.options.Results.OFF_TIME;
+            microscope_rate = obj.SAMPLING_PERIOD;
+            off_time = obj.OFF_TIME;
             microscope_frames = 0:microscope_rate:obj.behavior_raw.time(end);
             
             fn = fieldnames(obj.behavior_raw);
@@ -161,13 +332,10 @@ classdef NeurotarExtractor < handle
                 end
                 
             end
-            
-            obj.behavior.pose = [obj.behavior.X; obj.behavior.Y; ...
-                        wrapTo360(obj.behavior.alpha - 90)];
-            
+
         end
         
-        function movingDetector(obj)
+        function detectMovement(obj)
             
             % This function detects when the mouse is running and when is not
             % by detecting the moving bouts.
@@ -178,7 +346,7 @@ classdef NeurotarExtractor < handle
             speed = smooth(speed, 200, 'moving')';
             
             speed(speed>200) = 200;
-            speed_thresh = 8;
+            speed_thresh = 5;
             bout_thresh = 10;
             
             moving_time = speed > speed_thresh;
@@ -202,7 +370,6 @@ classdef NeurotarExtractor < handle
         
     end
     
-  
     % Statistic visualization
     methods (Access = private)
         
@@ -210,6 +377,13 @@ classdef NeurotarExtractor < handle
     
     % Saving methods
     methods
+        
+%         function save(obj)
+%             
+%             save('C:\My folder\filename','varname')
+%             
+%             
+%         end
         
     end
     
